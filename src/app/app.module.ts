34300ceca160307931936957e577e9d6bf581dca
Roberto <robertorebolledo151@gmail.com>
2023-10-31 10:57:55 +0100

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ImportModule } from './import-module/import/import.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ImportModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
