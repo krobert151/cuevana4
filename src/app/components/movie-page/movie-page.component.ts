fca3419d452b03fdef534cfbd660990f79b27368
Roberto <robertorebolledo151@gmail.com>
2023-11-03 10:06:02 +0100

    to home

--- src/app/components/movie-page/movie-page.component.ts
+++ src/app/components/movie-page/movie-page.component.ts
@@ -1,53 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieResponse } from 'src/app/modals/movie/movie.module';
+import { VideoResponse } from 'src/app/modals/video/video.module';
 import { MovieListService } from 'src/app/service/movie-list.service';
 import { environment } from 'src/environments/environment';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-movie-page',
   templateUrl: './movie-page.component.html',
   styleUrls: ['./movie-page.component.css']
 })
-export class MoviePageComponent implements OnInit{
+export class MoviePageComponent implements OnInit {
 
-  movie!:MovieResponse;
-  id!:string|null;
-  constructor(private movieService: MovieListService,private route:ActivatedRoute){}
+  movie!: MovieResponse;
+  video!: VideoResponse;
+  videoUrl!: SafeResourceUrl;
+  id!: string | null;
+  constructor(private movieService: MovieListService, private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    this.id= this.route.snapshot.paramMap.get('id');
-    if(this.id!=null){
-      this.movieService.getMovieById(this.id).subscribe(resp=>{
-        this.movie=resp;
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id != null) {
+      this.movieService.getMovieById(this.id).subscribe(resp => {
+        this.movie = resp;
+      })
+      this.movieService.getVideoById(this.id).subscribe(resp => {
+        this.video = resp;
       })
     }
+
   }
-  getMovieBackground(){
+  getMovieBackground() {
     return (`${environment.Photoheader}${this.movie.backdrop_path}`)
   }
-  getMoviePoster(){
+  getMoviePoster() {
     return (`${environment.Photoheader}${this.movie.poster_path}`)
   }
-  getMovieAnio(){
+  getMovieAnio() {
     return this.movie.release_date.split('-')[0];
   }
-  getMovieDate(){
+  getMovieDate() {
     return this.movie.release_date.split('-').join('/');
   }
-  getMovieGenders(){
+  getMovieGenders() {
     var movieGenders = [];
     for (let i = 0; i < this.movie.genres.length; i++) {
       movieGenders.push(this.movie.genres[i].name);
     }
     return movieGenders.join(' ,');
   }
-  getMovieDuration(){
+  getMovieDuration() {
     var totalminutes = this.movie.runtime;
-    var horas = totalminutes/60;
-    horas= parseInt(horas.toString());
-    var minutos = totalminutes%60;
-    return (horas+'h'+minutos+'m')
+    var horas = totalminutes / 60;
+    horas = parseInt(horas.toString());
+    var minutos = totalminutes % 60;
+    return (horas + 'h' + minutos + 'm')
+  }
+  getMovieTrailer(key: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`//www.youtube.com/embed/${key}?autoplay=0&amp;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=es&amp;modestbranding=1&amp;fs=1&amp;autohide=1`);
   }
 
 
--- src/app/service/movie-list.service.ts
+++ src/app/service/movie-list.service.ts
@@ -4,14 +4,14 @@ import { MovieListResponse } from '../modals/movie-list/movie-list.module';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { MovieResponse } from '../modals/movie/movie.module';
+import { VideoResponse } from '../modals/video/video.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieListService {
 
-  TOKENParam: string = '?api_key=5b1cf6fd754627940a36f56d2fa98242';
-  HeaderURL: string = 'https://api.themoviedb.org/3/'
+
 
   constructor(private https: HttpClient) { }
 
@@ -22,12 +22,16 @@ export class MovieListService {
   getMostRatedMovies(): Observable<MovieListResponse> {
     return this.https.get<MovieListResponse>(`${environment.HeadUrl}top_rated?api_key=${environment.ApiKey}`);
   }
-  getNowPlayingMovies(page:string): Observable<MovieListResponse> {
+  getNowPlayingMovies(page: string): Observable<MovieListResponse> {
     return this.https.get<MovieListResponse>(`${environment.HeadUrl}now_playing?api_key=${environment.ApiKey}&page=${page}`);
   }
 
-  getMovieById(id:string):Observable<MovieResponse>{
+  getMovieById(id: string): Observable<MovieResponse> {
     return this.https.get<MovieResponse>(`${environment.HeadUrl}${id}?api_key=${environment.ApiKey}`);
+  }
+
+  getVideoById(id: string): Observable<VideoResponse> {
+    return this.https.get<VideoResponse>(`${environment.HeadUrl}${id}/videos?api_key=${environment.ApiKey}`);
   }
 
 
